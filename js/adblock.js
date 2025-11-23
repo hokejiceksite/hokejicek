a8af30e0ae044ca611fc6c16ad6e30862b826d61
hokejiceksite <bilcusadam@gmail.com>
2025-11-23 08:29:29 +0100

    Update adblock.js

--- js/adblock.js
+++ js/adblock.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded",()=>{
     const overlay=document.getElementById("adblock-overlay");
+
     const bait=document.createElement("div");
     bait.style.width="1px";
     bait.style.height="1px";
@@ -7,21 +8,20 @@ document.addEventListener("DOMContentLoaded",()=>{
     bait.style.top="-1000px";
     bait.className="adsbox banner adunit ad ads advertisement";
     document.body.appendChild(bait);
+
     setTimeout(()=>{
         const hidden=
             bait.offsetHeight===0||
             bait.clientHeight===0||
             getComputedStyle(bait).display==="none"||
             getComputedStyle(bait).visibility==="hidden";
+
         bait.remove();
+
         if(hidden){
             overlay.style.display="flex";
         }else{
-            overlay.style.display="none";
             startStream();
         }
-    },300);
-});
-document.addEventListener("click",e=>{
-    if(e.target.id==="adblock-retry")location.reload();
+    },200);
 });
